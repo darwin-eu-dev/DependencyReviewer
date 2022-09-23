f28d1b6252054db64c33624a8ca7ae3b1c8a3b70
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2022-09-23 11:05:11 +0200

    Updated functionality to get package functions, added line where function is used

--- R/summariseFunctionUse.R
+++ R/summariseFunctionUse.R
@@ -1,10 +1,11 @@
 #' Summarise functions used in R package
 #'
+#'Deprecated
+#'
 #' @return tibble
-#' @export
 #'
 #' @examples
-summariseFunctionUse <- function() {
+DEP_summariseFunctionUse <- function() {
   r_files <- list.files(here::here("R"))
   deps_used <- list()
 
@@ -16,6 +17,96 @@ summariseFunctionUse <- function() {
 
   deps_used<-dplyr::bind_rows(deps_used) %>%
     dplyr::group_by(funs, pkgs) %>%
+    dplyr::tally() %>%
+    dplyr::arrange(desc(n))
+
+  return(deps_used)
+}
+
+
+#' funsUsedInLine
+#'
+#'Support function for funsUsedInFile
+#'
+#' @param file_txt file to use
+#' @param i line
+#' @param verbose Prints message when no function found
+#'
+#' @return data.frame of 3 colums: Package (pkg); Function (fun); Line in
+#' script (line)
+#'
+#' @examples
+funsUsedInLine <- function(file_txt, i, verbose=FALSE) {
+  line <- file_txt[i]
+
+  fun_vec <- unlist(stringr::str_extract_all(
+    string = line,
+    pattern = "(\\w+::(?:\\w+\\.)?\\w+\\(|(?:\\w+\\.)?\\w+\\()"))
+
+  fun_vec <- stringr::str_remove_all(
+    string = fun_vec,
+    pattern = "\\(")
+
+  fun_vec <- stringr::str_split(
+    string = fun_vec,
+    pattern = "::")
+
+  if(length(fun_vec) > 0) {
+    fun_vec <- lapply(
+      X = fun_vec,
+      FUN = function(x) {
+        if(length(x) == 1) {
+          x <- list("unknown", x)
+        } else {
+          list(x)
+        }
+      })
+
+    df <- data.frame(t(sapply(fun_vec, unlist)))
+    names(df) <- c("pkg", "fun")
+
+    df$pkg[df$fun %in% ls("package:base")] <- "base"
+    df$pkg[df$fun %in% ls("package:methods")] <- "methods"
+
+
+    df$line <- rep(i, dim(df)[1])
+    return(dplyr::tibble(df))
+
+  } else {
+    if(verbose == TRUE) {
+      message(glue::glue("No functions found for line: ", i))
+    }
+  }
+}
+
+
+#' funsUsedInFile
+#'
+#' @param files
+#'
+#' @return table
+#' @export
+#'
+#' @examples
+funsUsedInFile <- function(files) {
+  dplyr::bind_rows(lapply(X = files, FUN = function(file) {
+    file_txt <- readLines(here::here("R", file))
+    sapply(1:length(file_txt), funsUsedInLine, file_txt = file_txt)
+  }))
+}
+
+#' Summarise functions used in R package
+#'
+#'Deperecated
+#'
+#' @return tibble
+#'
+#' @examples
+summariseFunctionUse <- function(r_files) {
+  deps_used <- funsUsedInFile(r_files)
+
+  deps_used <- dplyr::bind_rows(deps_used) %>%
+    dplyr::group_by(fun, pkg, line) %>%
     dplyr::tally() %>%
     dplyr::arrange(desc(n))
 
