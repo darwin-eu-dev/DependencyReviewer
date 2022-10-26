77567f66576bdb3787ca9d423c11aacfe10e7e1f
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2022-10-26 15:35:21 +0200

    Moved code

--- R/darwinLint.R
+++ R/darwinLint.R
@@ -18,3 +18,19 @@ darwinLintPackage <- function() {
          might be to large to lint all together. Use: lintFile(fileName)")
   })
 }
+
+#' darwinLintFile
+#'
+#' Lint a given file.
+#'
+#' @import lintr
+#'
+#' @param fileName Path to file to lint
+#'
+#' @export
+lintFile <- function(fileName) {
+  lintr::lint(
+    filename = fileName,
+    linters = lintr::linters_with_defaults(
+      lintr::object_name_linter(styles = "camelCase")))
+}
