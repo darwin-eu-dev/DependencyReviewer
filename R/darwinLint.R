ada2d958351088bbf6386d80424a561e1b5bb6b6
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2022-10-26 15:31:44 +0200

    Added error check + message

--- R/darwinLint.R
+++ R/darwinLint.R
@@ -1,15 +1,20 @@
 #' darwinLint
 #'
-#' Darwin lintr object, using default lintr object with camelCase.
+#' Darwin lintr object, using default lintr object with camelCase
 #'
 #' @import lintr
 #' @export
 #'
 #' @examples
 #' darwinLint()
-darwinLint <- function() {
-  lintr::lint_package(
-    path = ".",
-    linters = lintr::linters_with_defaults(
-      lintr::object_name_linter(styles = "camelCase")))
+darwinLintPackage <- function() {
+  tryCatch({
+    lintr::lint_package(
+      path = ".",
+      linters = lintr::linters_with_defaults(
+        lintr::object_name_linter(styles = "camelCase")))
+  }, error = function(e) {
+    stop("Error was caught during the linting of your package. The package
+         might be to large to lint all together. Use: lintFile(fileName)")
+  })
 }
