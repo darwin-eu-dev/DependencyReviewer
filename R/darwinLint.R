c9fc6a508381594df3d84153afef0472ef3be7a2
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2022-11-02 15:20:57 +0100

    Added lint scoring

--- R/darwinLint.R
+++ R/darwinLint.R
@@ -35,3 +35,51 @@ darwinLintFile <- function(fileName) {
     linters = lintr::linters_with_defaults(
       lintr::object_name_linter(styles = "camelCase")))
 }
+
+
+#' darwinLintScore
+#'
+#' Function that scores the lintr output as a percentage per message type
+#' (style, warning, error). Lintr messages / lines assessed * 100
+#'
+#' @param lintFunction
+#'     Lint function to use
+#' @param ...
+#'     Other parameters a Lint function might need (i.e. file name)
+#' @export
+darwinLintScore <- function(lintFunction, ...) {
+  lintTable <- data.frame(lintFunction(...))
+  files <- unique(lintTable$filename)
+
+  nLines <- sum(unlist(lapply(
+    X = files,
+    FUN = function(file) {
+      length(readLines(file))
+    })))
+
+  pct <- lintTable %>%
+    group_by(.data$type) %>%
+    tally() %>%
+    summarise(.data$type, pct = round(n / nLines * 100, 2))
+
+  if (nrow(pct) == 0) {
+    cli::cli_alert_info(cli::col_green(
+      "{nrow(pct)} Lintr messages found"))
+  } else {
+    invisible(lapply(X = seq_len(nrow(pct)), FUN = function(i) {
+      if (pct[i, 1] == "error") {
+        cli::cli_alert_info(cli::col_red(
+          "{pct[i, 1]}: {pct[i, 2]}% of lines of code have linting messages"))
+      } else if (pct[i, 1] == "warning") {
+        cli::cli_alert_info(cli::col_yellow(
+          "{pct[i, 1]}: {pct[i, 2]}% of lines of code have linting messages"))
+      } else if (pct[i, 1] == "style") {
+        cli::cli_alert_info(cli::col_blue(
+          "{pct[i, 1]}: {pct[i, 2]}% of lines of code have linting messages"))
+      } else {
+        cli::cli_alert_info(cli::col_magenta(
+          "{pct[i, 1]}: {pct[i, 2]}% of lines of code have linting messages"))
+      }
+    }))
+  }
+}
