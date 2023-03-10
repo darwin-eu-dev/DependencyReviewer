29bfd2f024d85398d9957ff29aabf763b62306b5
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-03-10 11:02:01 +0100

    Updated tests

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -5,19 +5,19 @@ test_that("Lint file", {
   path <- normalizePath(system.file(
     package = "DependencyReviewer", "testScript.R"))
 
-  expect_type(darwinLintFile(path), "list")
+  expect_type(lintFile(path), "list")
 })
 
 test_that("Lint package", {
   path <- normalizePath(paste0(.Library, "/base"))
-  expect_type(darwinLintPackage(path = path), "list")
+  expect_type(lintPackage(path = path), "list")
 })
 
 test_that("Lint score file", {
   path <- normalizePath(system.file(
     package = "DependencyReviewer", "testScript.R"))
 
-  expect_s3_class(darwinLintScore(
-    lintFunction = darwinLintFile,
+  expect_s3_class(lintScore(
+    lintFunction = lintFile,
     path), "data.frame")
 })
