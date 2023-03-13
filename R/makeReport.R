c251f0a1a43145c07668e5c22f93c07bdd9821a0
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-03-13 12:22:41 +0100

    Added makeReport functionality

---
+++ R/makeReport.R
@@ -0,0 +1,30 @@
+#' makeReport
+#'
+#' Uses rmarkdown's render function to render a html-report of the given
+#' package.
+#'
+#' @param pkgPath Path to package folder.
+#' @param outputFile Path to html-file.
+#' @param showCode Default: FALSE; Boolean to show code or not in the report.
+#'
+#' @import rmarkdown
+#'
+#' @export
+#'
+#' @return NULL
+#' @examples
+#' if (interactive()) {
+#'   # Define pkgPath; Glue 1.6.2.9000 comes with the package.
+#'   pkgPath <- system.file(package = "DependencyReviewer", "glue")
+#'
+#'   # Define outputFile as a temp file.
+#'   outputFile <- tempfile(fileext = ".html")
+#'
+#'   makeReport(pkgPath, outputFile)
+#' }
+makeReport <- function(pkgPath, outputFile, showCode = FALSE) {
+  rmarkdown::render(
+    input = system.file(package = "DependencyReviewer", "rmd", "report.Rmd"),
+    output_file = outputFile,
+    params = list(pkgPath = pkgPath, showCode = showCode))
+}
